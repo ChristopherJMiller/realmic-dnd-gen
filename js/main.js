25734781cccf5b96f1a3745953a0a3917fae6102
Christopher Miller <admin@mctherealm.net>
2016-02-07 18:15:39 -0500

    Added class bonuses, made separate js file for classes

---
+++ js/main.js
@@ -0,0 +1,11 @@
+"use strict";
+class StatSet {
+  constructor (str, con, dex, int, wis, chr) {
+    this.str = str;
+    this.con = con;
+    this.dex = dex;
+    this.int = int;
+    this.wis = wis;
+    this.chr = chr;
+  }
+}
